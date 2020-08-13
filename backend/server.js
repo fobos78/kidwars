ade3995d4b2c95bec5c547b2e6b47797d98e5ba8
shell <shellipov@mail.ru>
2020-08-13 14:31:53 +0300

    rekease

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,42 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import fS from 'session-file-store';
+
+import indexRouter from './routes/index.js';
+import loginRouter from './routes/login.js';
+import singInRouter from './routes/sing_in.js';
+import taskRouter from './routes/task.js';
+import editRouter from './routes/edit.js';
+
+dotenv.config();
+const fileStore = fS(session);
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/api', (req, res) => {
-  res.json({ test: 'OK' });
-});
+app.use(
+  session({
+    store: new fileStore(),
+    key: 'user_sid',
+    secret: 'something',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 1000 * 60 * 60 * 24 * 365,
+    },
+  }),
+);
+
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/login', loginRouter);
+app.use('/sing_in', singInRouter);
+app.use('/task', taskRouter);
+app.use('/edit', editRouter);
 
 app.listen(process.env.PORT ?? 3001);
