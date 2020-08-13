efaa6e454dfc8b39a8979c33223824a069d7e4b0
Artem <marsov78@mail.ru>
2020-08-13 19:33:50 +0300

    bot1.1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,16 +9,11 @@ function App() {
     <Provider store={store}>
       <Router>
         <Switch>
-<<<<<<< HEAD
           <Route path="/" />
         </Switch>
         <Switch>
           <Route path="/singin">
             <SingIn />
-=======
-          <Route path="/">
-            hello
->>>>>>> e3b3b4112eb75d987a7a1f0c2882d9fb699bece9
           </Route>
         </Switch>
       </Router>
