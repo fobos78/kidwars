533ce7936b932a8b0f4510344a6d46299a002f39
Vitalik91 <cool.donchenko@gmail.com>
2020-08-18 18:58:31 +0300

    newSeed

--- backend/model/card.js
+++ backend/model/card.js
@@ -2,14 +2,25 @@ import mongoose from 'mongoose';
 
 const { Schema, model } = mongoose;
 
+// const cardSchema = new Schema({
+//   theme: String,
+//   points: Number,
+//   question: String,
+//   answer1: String,
+//   answer2: String,
+//   answer3: String,
+//   answerTrue: String,
+//   classNumber: Number,
+// });
+
 const cardSchema = new Schema({
   theme: String,
-  points: Number,
+  classNumber: Number,
+  fourth: Number,
   question: String,
-  answer1: String,
-  answer2: String,
-  answer3: String,
+  answerOptions: [String],
   answerTrue: String,
+  creator: String,
 });
 
 export default model('Card', cardSchema);
