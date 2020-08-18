b58283867f652ff627525c8faa23f10c9de1c1a1
Vitalik91 <cool.donchenko@gmail.com>
2020-08-18 15:30:52 +0300

    new

--- frontend/src/Components/Config/Config.js
+++ frontend/src/Components/Config/Config.js
@@ -3,9 +3,8 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-// import { sendCategory } from '../../redux/actions';
 import './Config.css';
+import Modal from '../Modal/Modal';
 
 function Config() {
   const [classNumber, setClassNumber] = useState(1);
@@ -13,13 +12,13 @@ function Config() {
   const [theme, setTheme] = useState('Математика');
   const [needScore, setNeedScore] = useState('');
   const [info, setInfo] = useState({});
+  const [flag, setFlag] = useState(true);
 
   const userEmail = useSelector((state) => state.user.email);
 
   function changeInput(event) {
     setNeedScore(event.target.value);
   }
-
 
   async function sendConfig(event) {
     event.preventDefault();
@@ -43,61 +42,64 @@ function Config() {
     setInfo(result.taskConfig);
   }
 
-  useEffect(() => {
-    (async () => {
-      const response = await fetch(`/config/${userEmail}`);
-      const result = await response.json();
-      setInfo(result);
-    })();
-  }, [sendConfig]);
+  // useEffect(() => {
+  //   (async () => {
+  //     const response = await fetch(`/config/${userEmail}`);
+  //     const result = await response.json();
+  //     setInfo(result);
+  //   })();
+  // }, [sendConfig]);
 
   return (
-    <div className="Config">
+    <>
+      <div className="Config">
+        {flag && <Modal flag={flag} setFlag={setFlag} />}
 
-      <form onSubmit={sendConfig} name="form">
-        <p>
-          <button className="button" type="button">Класс</button>
-          <button onClick={() => setClassNumber(1)} className="button" type="button">1</button>
-          <button onClick={() => setClassNumber(2)} className="button" type="button">2</button>
-          <button onClick={() => setClassNumber(3)} className="button" type="button">3</button>
-          <button onClick={() => setClassNumber(4)} className="button" type="button">4</button>
-        </p>
-        <p>
-          Четверть:
+        <form onSubmit={sendConfig} name="form">
+          <p>
+            <button className="button" type="button">Класс</button>
+            <button onClick={() => setClassNumber(1)} className="button" type="button">1</button>
+            <button onClick={() => setClassNumber(2)} className="button" type="button">2</button>
+            <button onClick={() => setClassNumber(3)} className="button" type="button">3</button>
+            <button onClick={() => setClassNumber(4)} className="button" type="button">4</button>
+          </p>
+          <p>
+            Четверть:
           <button onClick={() => setFourth(1)} className="button" type="button">1</button>
-          <button onClick={() => setFourth(2)} className="button" type="button">2</button>
-          <button onClick={() => setFourth(3)} className="button" type="button">3</button>
-          <button onClick={() => setFourth(4)} className="button" type="button">4</button>
-        </p>
-        <p>
-          Дисциплина:
+            <button onClick={() => setFourth(2)} className="button" type="button">2</button>
+            <button onClick={() => setFourth(3)} className="button" type="button">3</button>
+            <button onClick={() => setFourth(4)} className="button" type="button">4</button>
+          </p>
+          <p>
+            Дисциплина:
           <button onClick={() => setTheme('Математика')} className="button2" type="button">Математика</button>
-          <button onClick={() => setTheme('Физика')} className="button2" type="button">Физика</button>
-          <button onClick={() => setTheme('Анг. язык')} className="button2" type="button">Анг. язык</button>
-          <button onClick={() => setTheme('Литература')} className="button2" type="button">Литература</button>
-        </p>
-        <p>
-          <label htmlFor="needScore">Количество очков необходимых для доступа: </label>
-          <input required onChange={changeInput} type="text" name="needScore" id="needScore" value={needScore} />
-        </p>
-        <button className="button" type="submit">Сохранить изменения</button>
-      </form>
-      <div>
-        {info.userName}
-        <br />
-        {info.kidName}
-        <br />
-        {info.classNumber}
-        <br />
-        {info.fourth}
-        <br />
-        {info.theme}
-        <br />
-        {info.needScore}
-        <br />
-        {info.score}
+            <button onClick={() => setTheme('Физика')} className="button2" type="button">Физика</button>
+            <button onClick={() => setTheme('Анг. язык')} className="button2" type="button">Анг. язык</button>
+            <button onClick={() => setTheme('Литература')} className="button2" type="button">Литература</button>
+          </p>
+          <p>
+            <label htmlFor="needScore">Количество очков необходимых для доступа: </label>
+            <input required onChange={changeInput} type="text" name="needScore" id="needScore" value={needScore} />
+          </p>
+          <button className="button" type="submit">Сохранить изменения</button>
+        </form>
+        <div>
+          {info.userName}
+          <br />
+          {info.kidName}
+          <br />
+          {info.classNumber}
+          <br />
+          {info.fourth}
+          <br />
+          {info.theme}
+          <br />
+          {info.needScore}
+          <br />
+          {info.score}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ frontend/src/Components/Modal/Modal.js
@@ -0,0 +1,52 @@
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import './Modal.css';
+
+function Modal({ flag, setFlag }) {
+
+  const [textError, setTextError] = useState('');
+  const userEmail = useSelector((state) => state.user.email);
+
+  async function changeFlag(event) {
+    event.preventDefault();
+    const password = event.target.input.value;
+
+    const response = await fetch('/config/access', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        userEmail,
+        password,
+      }),
+    });
+    const result = await response.json();
+    if (result.password) {
+      setFlag(!flag);
+    }
+    setTextError('Неверный пароль');
+  }
+
+  return (
+    <div className="bodyAllModal">
+      <div className="Modal">
+        <br />
+        <p className="TextConfig">Для доспупа к настройкам введите пароль</p>
+        <form onSubmit={changeFlag}>
+          <input type="password" name="input" />
+          <br />
+          <br />
+          <button type="submit">Подтвердить пароль</button>
+        </form>
+        <br />
+        <p className="TextError">
+          {textError}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
